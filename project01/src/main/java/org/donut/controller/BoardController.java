b50034c5f0850350c9eb01a3bb903232b06c0f97
journeytorainbow <58691899+journeytorainbow@users.noreply.github.com>
2020-12-20 23:24:34 +0900

    Add control layer

---
+++ project01/src/main/java/org/donut/controller/BoardController.java
@@ -0,0 +1,17 @@
+package org.donut.controller;
+
+import org.donut.service.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.*;
+import lombok.extern.log4j.*;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+	
+}
