14fcc7e4a2352e55963bf2f8da84a0e9b10af567
journeytorainbow <58691899+journeytorainbow@users.noreply.github.com>
2020-12-21 17:22:14 +0900

    Implement CRUD method for control layer

--- project01/src/main/java/org/donut/controller/BoardController.java
+++ project01/src/main/java/org/donut/controller/BoardController.java
@@ -1,12 +1,18 @@
 package org.donut.controller;
 
+import java.lang.ProcessBuilder.*;
+import java.util.*;
+
+import org.donut.domain.*;
 import org.donut.service.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.*;
 
 import lombok.*;
 import lombok.extern.log4j.*;
+import oracle.jdbc.proxy.annotation.*;
 
 @Controller
 @Log4j
@@ -14,4 +20,56 @@ import lombok.extern.log4j.*;
 @AllArgsConstructor
 public class BoardController {
 	
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		
+		log.info("list");
+		model.addAttribute("list", service.getList());
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+	
+		log.info("register : " + board);
+		
+		service.register(board);
+		
+		rttr.addFlashAttribute("result", board.getBno());
+		
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/read")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		
+		log.info("/read");
+		model.addAttribute("board", service.get(bno));
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		
+		log.info("modify : " + board);
+		
+		if (service.modify(board)) {
+			
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		
+		log.info("remove..." + bno);
+		
+		if(service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+		return "redirect:/board/list";
+	}
 }
---
+++ project01/src/test/java/org/donut/controller/BoardControllerTests.java
@@ -0,0 +1,89 @@
+package org.donut.controller;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.test.context.*;
+import org.springframework.test.context.junit4.*;
+import org.springframework.test.context.web.*;
+import org.springframework.test.web.servlet.*;
+import org.springframework.test.web.servlet.request.*;
+import org.springframework.test.web.servlet.setup.*;
+import org.springframework.web.context.*;
+
+import lombok.*;
+import lombok.extern.log4j.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration // test for controller
+@ContextConfiguration({
+	"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+	"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})
+@Log4j
+public class BoardControllerTests {
+	
+	@Setter(onMethod_ = {@Autowired})
+	private WebApplicationContext ctx;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	
+	@Test
+	public void testList() throws Exception {
+		
+		log.info(
+			mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))
+			.andReturn()
+			.getModelAndView()
+			.getModelMap());
+	}
+	
+	@Test
+	public void testRegister() throws Exception {
+		
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/register")
+				.param("title", "테스트 새글 제목")
+				.param("content", "테스트 새글 내용")
+				.param("writer", "user00")
+			).andReturn().getModelAndView().getViewName();
+	
+		log.info(resultPage);
+	}
+	
+	@Test
+	public void testRead() throws Exception {
+		
+		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/read")
+			.param("bno", "154"))
+			.andReturn()
+			.getModelAndView().getModelMap());
+	}
+	
+	@Test
+	public void testModify() throws Exception {
+		
+		String resultPage = mockMvc
+			.perform(MockMvcRequestBuilders.post("/board/modify")
+				.param("bno", "154")
+				.param("title", "수정된 테스트 새글 제목")
+				.param("content", "수정된 테스트 새글 내용")
+				.param("writer", "user00"))
+			.andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+	}
+	
+	@Test
+	public void testRemove() throws Exception {
+		
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove")
+			.param("bno", "145"))
+			.andReturn().getModelAndView().getViewName();
+		
+		log.info(resultPage);
+	}
+}
