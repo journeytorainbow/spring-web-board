c60accdf7a9e956615e30993419225e714c66a38
journeytorainbow <58691899+journeytorainbow@users.noreply.github.com>
2021-01-03 00:23:24 +0900

    Implement service layer for reply

---
+++ project01/src/main/java/org/donut/service/RelyServiceImpl.java
@@ -0,0 +1,54 @@
+package org.donut.service;
+
+import java.util.*;
+
+import org.donut.domain.*;
+import org.donut.mapper.*;
+import org.springframework.stereotype.*;
+
+import lombok.*;
+import lombok.extern.log4j.*;
+
+@Service
+@Log4j
+@AllArgsConstructor
+public class RelyServiceImpl implements ReplyService {
+	
+//	@Setter(onMethod_ = @Autowired)
+	private ReplyMapper mapper;
+
+	@Override
+	public int register(ReplyVO vo) {
+		
+		log.info("register...." + vo);
+		return mapper.insert(vo);
+	}
+
+	@Override
+	public ReplyVO get(Long rno) {
+		
+		log.info("get...." + rno);
+		return mapper.read(rno);
+	}
+
+	@Override
+	public int modify(ReplyVO vo) {
+		
+		log.info("modify...." + vo);
+		return mapper.update(vo);
+	}
+
+	@Override
+	public int remove(Long rno) {
+		
+		log.info("remove...." + rno);
+		return mapper.delete(rno);
+	}
+
+	@Override
+	public List<ReplyVO> getList(Criteria cri, Long bno) {
+		
+		log.info("get Reply List of Board " + bno);
+		return mapper.getListWithPaging(cri, bno);
+	}
+}
---
+++ project01/src/main/java/org/donut/service/ReplyService.java
@@ -0,0 +1,18 @@
+package org.donut.service;
+
+import java.util.*;
+
+import org.donut.domain.*;
+
+public interface ReplyService {
+	
+	public int register(ReplyVO vo);
+	
+	public ReplyVO get(Long rno);
+	
+	public int modify(ReplyVO vo);
+	
+	public int remove(Long rno);
+	
+	public List<ReplyVO> getList(Criteria cri, Long bno);
+}
