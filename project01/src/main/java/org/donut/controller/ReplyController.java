5a8aabb850d31f45cc0aef57eb11b82fc55ee865
journeytorainbow <journeytorainbow@github.com>
2021-01-03 22:29:26 +0900

    Add RUD code to presentation layer for reply

--- project01/src/main/java/org/donut/controller/ReplyController.java
+++ project01/src/main/java/org/donut/controller/ReplyController.java
@@ -1,5 +1,7 @@
 package org.donut.controller;
 
+import java.util.*;
+
 import org.donut.domain.*;
 import org.donut.service.*;
 import org.springframework.http.*;
@@ -26,5 +28,42 @@ public class ReplyController {
 		log.info("Reply INSERT COUNT : " + insertCount);
 		
 		return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	
+	// 특정 게시물의 댓글 목록 확인
+	@GetMapping(value = "/pages/{bno}/{page}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<List<ReplyVO>> getList(@PathVariable("page") int page, @PathVariable("bno") Long bno) {
+		
+		log.info("getList...........");
+		Criteria cri = new Criteria(page, 10);
+		log.info(cri);
+		
+		return new ResponseEntity<>(service.getList(cri,  bno), HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/{rno}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno) {
+		
+		log.info("get : " + rno);
+		
+		return new ResponseEntity<>(service.get(rno), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{rno}", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+		
+		log.info("remove : " + rno);
+		
+		return service.remove(rno) == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+ 	}
+	
+	@RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH}, value = "/{rno}", consumes = "application/json", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> modify(@RequestBody ReplyVO vo, @PathVariable("rno") Long rno) {
+		
+		vo.setRno(rno);
+		log.info("rno : " + rno);
+		log.info("modify : " + rno);
+		return service.modify(vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
